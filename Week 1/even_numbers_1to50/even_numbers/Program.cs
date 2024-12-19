016186a7e9e6a9d1b8ac05d261aa804d61d235d9
DHANUSH <114476777+Dhanush-123@users.noreply.github.com>
2024-12-19 18:54:03 +0530

    Add files via upload

---
+++ Week 1/even_numbers_1to50/even_numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Even numbers between 1 and 50:");
+        for (int i = 2; i <= 50; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
